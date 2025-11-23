8755f003e6632f8b48c0f3dd110aaa7241d320b3
victorCNeves <concolato.neves@gmail.com>
2025-11-23 11:09:43 -0300

    add auth-token middleware

---
+++ backend/src/middleware/auth-token.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const secretKey = 'your_secret_key';
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = authenticateToken;
