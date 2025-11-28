7e45c0ebbc5b5d03581605c48760b19793fabfa5
victorCNeves <concolato.neves@gmail.com>
2025-11-28 14:54:59 -0300

    alteração permissao rotas

--- backend/src/routers/route.js
+++ backend/src/routers/route.js
@@ -20,16 +20,16 @@ db.sequelize.sync({force: true}).then(() => {
 router.post('/login', authController.login)
 
 router.get('/usuarios', authToken, authorization(USER_TYPES.ADMIN), userController.getUsers);
-router.get('/usuarios/:id', authToken, authorization(USER_TYPES.ADMIN), userController.getUsersById);
+router.get('/usuarios/:id', authToken, authorization(USER_TYPES.CLIENTE), userController.getUsersById);
 router.post('/usuarios', authToken, authorization(USER_TYPES.ADMIN), userController.postUser);
-router.put('/usuarios/:id', authToken, authorization(USER_TYPES.ADMIN), userController.putUser);
+router.put('/usuarios/:id', authToken, authorization(USER_TYPES.CLIENTE), userController.putUser);
 router.delete('/usuarios/:id', authToken, authorization(USER_TYPES.ADMIN), userController.deleteUser);
 
 router.get('/clientes', authToken, authorization(USER_TYPES.FUNCIONARIO), clienteController.getClientes);
 router.get('/clientes/:id', authToken, authorization(USER_TYPES.CLIENTE), clienteController.getClienteById);
 router.post('/clientes', authToken, authorization(USER_TYPES.FUNCIONARIO), clienteController.postCliente);
 router.put('/clientes/:id', authToken, authorization(USER_TYPES.CLIENTE), clienteController.putCliente);
-router.delete('/clientes/:id', authToken, authorization(USER_TYPES.CLIENTE), clienteController.deleteCliente);
+router.delete('/clientes/:id', authToken, authorization(USER_TYPES.FUNCIONARIO), clienteController.deleteCliente);
 
 router.get('/quartos', quartoController.getQuartos);
 router.get('/quartos/:id', quartoController.getQuartoById);
@@ -39,8 +39,8 @@ router.delete('/quartos/:id', authToken, authorization(USER_TYPES.FUNCIONARIO), 
 
 router.get('/reservas', authToken, authorization(USER_TYPES.CLIENTE), reservaController.getReservas);
 router.get('/reservas/:id', authToken, authorization(USER_TYPES.CLIENTE), reservaController.getReservaById);
-router.post('/reservas', authToken, authorization(USER_TYPES.FUNCIONARIO), reservaController.postReserva);
-router.put('/reservas/:id', authToken, authorization(USER_TYPES.FUNCIONARIO), reservaController.putReserva);
+router.post('/reservas', authToken, authorization(USER_TYPES.CLIENTE), reservaController.postReserva);
+router.put('/reservas/:id', authToken, authorization(USER_TYPES.CLIENTE), reservaController.putReserva);
 router.delete('/reservas/:id', authToken, authorization(USER_TYPES.FUNCIONARIO), reservaController.deleteReserva);
 
 module.exports = router;
