ce250733694d0266a7b11e3ae226736c8e0d3085
victorCNeves <concolato.neves@gmail.com>
2025-11-29 16:21:30 -0300

    mudança relações para nao permitir null em reserva e padronizar fk

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -15,10 +15,10 @@ db.Quarto = require('../models/quarto')(sequelize,Sequelize);
 db.Reserva = require('../models/reserva')(sequelize,Sequelize);
 
 db.Usuario.hasOne(db.Cliente, {foreignKey: {name: 'id_usuario', allowNull: true, unique: true}, onDelete: 'CASCADE', onUpdate: 'CASCADE'});
-db.Cliente.belongsTo(db.Usuario);
-db.Cliente.hasMany(db.Reserva, {foreignKey: 'id_cliente', onDelete: 'CASCADE', onUpdate: 'CASCADE'});
-db.Reserva.belongsTo(db.Cliente);
-db.Quarto.hasMany(db.Reserva, {foreignKey: 'id_quarto', onDelete: 'CASCADE', onUpdate: 'CASCADE'});
-db.Reserva.belongsTo(db.Quarto);
+db.Cliente.belongsTo(db.Usuario, {foreignKey: 'id_usuario'});
+db.Cliente.hasMany(db.Reserva, {foreignKey: {name: 'id_cliente', allowNull: false}, onDelete: 'CASCADE', onUpdate: 'CASCADE'});
+db.Reserva.belongsTo(db.Cliente, {foreignKey: 'id_cliente'});
+db.Quarto.hasMany(db.Reserva, {foreignKey: {name: 'id_quarto', allowNull: false}, onDelete: 'CASCADE', onUpdate: 'CASCADE'});
+db.Reserva.belongsTo(db.Quarto, {foreignKey: 'id_quarto'});
 
 module.exports = db;
